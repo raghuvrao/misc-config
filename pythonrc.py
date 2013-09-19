95e4984a78011f88f03418c73d00d6a07bad8095
Raghu V. Rao <raghu.v.rao@gmail.com>
2013-09-19 14:34:15 -0500

    Set up history for interactive python sessions

--- pythonrc.py
+++ pythonrc.py
@@ -1,4 +1,18 @@
+import atexit
+import os
 import readline
 import rlcompleter
 
 readline.parse_and_bind('tab: complete')
+
+history_file = os.path.expanduser('~/.python_history')
+readline.set_history_length(1000)
+if os.path.exists(history_file):
+    readline.read_history_file(history_file)
+
+def save_history_file(history_file=history_file, readline=readline):
+    readline.write_history_file(history_file)
+
+atexit.register(save_history_file)
+
+del atexit, os, readline, rlcompleter, history_file, save_history_file
