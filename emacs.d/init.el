6ff4ae61d9b448f78327cab123da23c9a5f42e7a
Raghu Rao <raghu.v.rao@gmail.com>
2017-02-12 00:31:49 -0800

    emacs.d/init.el: reorganize + keybindings
    Use windmove defaults

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,12 +1,19 @@
 (when (>= emacs-major-version 24)
   (require 'package)
-  (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			   ("melpa" . "https://melpa.org/packages/")))
+  (setq package-archives
+	'(("gnu" . "https://elpa.gnu.org/packages/")
+	  ("melpa-stable" . "https://stable.melpa.org/packages/")
+	  ("melpa" . "https://melpa.org/packages/")))
+  (setq package-archive-priorities
+	'(("gnu" . 90)
+	  ("melpa-stable" . 70)
+	  ("melpa" . 50)))
+  (setq package-enable-at-startup nil)
   (package-initialize))
 
 (global-font-lock-mode -1)
 (electric-pair-mode -1)
-(menu-bar-mode 1)
+(menu-bar-mode 1)  ; Helps me to discover emacs.
 (line-number-mode 1)
 (column-number-mode 1)
 (show-paren-mode 1)
@@ -15,82 +22,70 @@
       make-backup-files nil
       frame-background-mode 'light
       transient-mark-mode t
-      auto-save-default t)
+      auto-save-default t
+      scroll-step 1)
 
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
+  (scroll-bar-mode 1))  ; I think I like them.
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
-(require 'auto-complete)
-(global-auto-complete-mode 1)
-
-(if (eq 'system-type 'darwin)
-    (progn
-      (require 'exec-path-from-shell)
-      (exec-path-from-shell-initialize)))
-
 (with-eval-after-load 'go-mode
-  (setq gofmt-command (or (executable-find "goimports")
-			  (executable-find "gofmt")))
-  (add-hook 'go-mode-hook
-	    '(lambda ()
-	       (setq show-trailing-whitespace t)
-	       (add-hook 'before-save-hook 'gofmt-before-save)
-	       (if (not (string-match "^go" compile-command))
-		   (set (make-local-variable 'compile-command)
-			"go build && go vet"))))
   (require 'go-guru)
-  (add-hook 'go-mode-hook 'go-guru-hl-identifier-mode)
   (require 'go-rename)
   (require 'go-errcheck)
   (require 'go-eldoc)
-  (add-hook 'go-mode-hook 'go-eldoc-setup))
+  (require 'auto-complete)
+  (require 'go-autocomplete)
+  (setq gofmt-command (or (executable-find "goimports")
+			  (executable-find "gofmt")))
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'go-mode-hook 'go-eldoc-setup)
+  (add-hook 'go-mode-hook 'go-guru-hl-identifier-mode)
+  (add-hook 'go-mode-hook '(lambda ()
+			     (auto-complete-mode 1)
+			     (setq show-trailing-whitespace t)
+			     (if (not (string-match "^go" compile-command))
+				 (set (make-local-variable 'compile-command)
+				      "go build && go vet")))))
 
-(defun my-after-make-frame-hook (&rest frame)
+(defun my-frame-misc-stuff-hook (&rest frame)
   (when (display-graphic-p)
-    (setq frame-title-format (concat "%b - emacs@" (system-name)))
+    (setq frame-title-format (concat "%b - " (system-name)))
+    (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
+    (setq cursor-type 'bar)
+    (global-hl-line-mode 1)
     (let ((f (if (car frame)
 		 (car frame)
 	       (selected-frame))))
-      (progn
-	(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
-	(set-face-background 'default "#FFFFFF" f)
-	(set-face-foreground 'default "#333333" f)
-	(set-face-background 'isearch "#EECCEE" f)
-	(set-face-foreground 'isearch nil f)
-	(set-face-background 'region "#CCEEEE" f)
-	(set-face-foreground 'region nil f)
-	(set-face-background 'show-paren-match "#FFFF88" f)
-	(set-face-foreground 'show-paren-match nil f)
-	(global-hl-line-mode 1)
-	(set-face-background 'hl-line "#EEEEEE" f)
-	(set-face-foreground 'hl-line nil f)))))
-(add-hook 'after-make-frame-functions 'my-after-make-frame-hook t)
-(add-hook 'after-init-hook 'my-after-make-frame-hook t)
+      (progn (set-face-background 'isearch "#EECCEE" f)
+	     (set-face-foreground 'isearch nil f)
+	     (set-face-background 'region "#FFEEBB" f)
+	     (set-face-background 'show-paren-match "#88EE88" f)
+	     (set-face-background 'hl-line "#EEEEEE" f)))))
+(add-hook 'after-make-frame-functions 'my-frame-misc-stuff-hook t)
+(add-hook 'after-init-hook 'my-frame-misc-stuff-hook t)
 
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
-(global-set-key (kbd "M-<up>") 'previous-multiframe-window)
-(global-set-key (kbd "M-<down>") 'next-multiframe-window)
-(global-set-key (kbd "ESC <up>") 'previous-multiframe-window)
-(global-set-key (kbd "ESC <down>") 'next-multiframe-window)
+(require 'windmove)
+(windmove-default-keybindings)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.  If you edit it by hand,
+ ;; you could mess it up, so be careful.  Your init file should
+ ;; contain only one such instance.  If there is more than one, they
+ ;; won't work right.
  '(package-selected-packages
-   (quote
-    (go-rename go-guru go-errcheck go-eldoc exec-path-from-shell auto-complete))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '(go-autocomplete
+     go-mode
+     go-rename
+     go-guru
+     go-errcheck
+     go-eldoc
+     auto-complete)))
