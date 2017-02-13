ec81b3c38ed6656c51a770e0d5e19e9a5a960c45
Raghu Rao <raghu.v.rao@gmail.com>
2017-02-12 19:29:52 -0800

    emacs.d/init.el: leave the custom-* stuff alone
    Custom keeps overwriting it anyway.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -77,15 +77,16 @@
 (windmove-default-keybindings)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.  If you edit it by hand,
- ;; you could mess it up, so be careful.  Your init file should
- ;; contain only one such instance.  If there is more than one, they
- ;; won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(go-autocomplete
-     go-mode
-     go-rename
-     go-guru
-     go-errcheck
-     go-eldoc
-     auto-complete)))
+   (quote
+    (go-autocomplete go-mode go-rename go-guru go-errcheck go-eldoc auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
