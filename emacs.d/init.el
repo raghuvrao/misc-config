4967aeccbbe00dca2ca95998d77b0fcc3bc18c8d
Raghu V. Rao <raghu.v.rao@gmail.com>
2017-02-19 23:23:39 -0800

    emacs config changes
    - disable hl-line-mode
    - add a function+keybinding to create a temporary buffer
    - make mode-line a little larger, to fit characters like g, y, p etc. properly

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,7 +11,6 @@
 
 (defun my-frame-misc-stuff-hook (&rest frame)
   (when (display-graphic-p)
-    (global-hl-line-mode 1)
     (setq frame-title-format (concat "%b - " (system-name)))))
 (add-hook 'after-make-frame-functions 'my-frame-misc-stuff-hook t)
 (add-hook 'after-init-hook 'my-frame-misc-stuff-hook t)
@@ -22,6 +21,14 @@
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+(defun raghu-get-temporary-buffer ()
+  "Switch to the buffer named \"*raghu-temporary*\".
+
+If the buffer does not already exist, create it and then switch to it."
+  (interactive)
+  (switch-to-buffer (get-buffer-create "*raghu-temporary*")))
+(global-set-key (kbd "C-c r t") 'raghu-get-temporary-buffer)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -49,9 +56,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((type x)) (:background "white" :foreground "grey20" :height 98 :family "DejaVu Sans Mono"))))
+ '(default ((((type x)) (:background "white" :foreground "black" :height 98 :family "DejaVu Sans Mono"))))
  '(fringe ((((type x)) (:background "grey95" :foreground "grey"))))
- '(hl-line ((((type x)) (:background "lemon chiffon"))))
  '(isearch ((((type x)) (:background "pink"))))
+ ;; '(mode-line ((((class color) (min-colors 88)) (:background "grey75" :foreground "black" :box (:line-width 1 :style released-button))) (t (:inverse-video t))))
+ '(mode-line ((((type x)) (:background "grey75" :foreground "black" :box (:line-width 1 :style released-button))) (t (:inverse-video t))))
+ ;; '(mode-line-inactive ((t (:inherit mode-line :background "grey90" :foreground "grey45" :box (:line-width 1 :color "grey75") :weight light))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "grey90" :foreground "grey45" :box (:line-width 1 :color "grey75") :weight light))))
  '(region ((((type x)) (:background "moccasin"))))
  '(show-paren-match ((((type x)) (:background "pale turquoise" :underline t)))))
