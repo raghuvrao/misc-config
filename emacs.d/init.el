42a911c6f9034c4e4388a96c42cd2c6d93eba4e1
Raghu V. Rao <raghu.v.rao@gmail.com>
2017-03-04 17:50:46 -0800

    - syntax highlighting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,6 +4,15 @@
 (column-number-mode 1)
 (setenv "PAGER" "cat")
 (show-paren-mode 1)
+(global-hl-line-mode 1)
+
+;; Enable syntax highlighting only in some places.
+(global-font-lock-mode -1)
+(add-hook 'diff-mode-hook #'font-lock-mode)
+(add-hook 'special-mode-hook #'font-lock-mode)
+
+;; Create a keybinding to enable syntax highlighting when needed.
+(global-set-key (kbd "C-c r f") #'font-lock-mode)
 
 (when (fboundp #'tool-bar-mode)
   (tool-bar-mode -1))
@@ -17,7 +26,7 @@
 (global-set-key (kbd "S-C-<up>") #'enlarge-window)
 
 ;; Unbind `C-x C-c' so I do not accidentally kill emacs.  I will
-;; instead use `M-x s a - t RET' instead.  It runs
+;; instead use `M-x sa-t RET' instead.  It runs
 ;; save-buffers-kill-terminal, which is bound by default to `C-x C-c'.
 (define-key global-map (kbd "C-x C-c") nil)
 
@@ -46,4 +55,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((type x)) (:height 100)))))
+ '(default ((((type x pm w32 ns)) (:height 90 :family "DejaVu Sans Mono"))))
+ '(region ((((type x pm w32 ns) (background light)) (:background "LightGoldenRod2")))))
