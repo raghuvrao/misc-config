a79b1a357557a72aa4258c5ab9cee8f6287fae42
Raghu V. Rao <raghu.v.rao@gmail.com>
2017-04-11 12:41:01 -0700

    emacs conf: remove a few undesirable key-bindings.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,6 +9,13 @@
 (when (fboundp #'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp #'scroll-bar-mode) (scroll-bar-mode -1))
 
+;; Remove a few undesirable key-bindings.  I will repurpose some of
+;; them.
+(define-key global-map (kbd "C-x C-c") nil)
+(define-key global-map (kbd "C-z") nil)
+(define-key global-map (kbd "C-x C-z") nil)
+(define-key global-map (kbd "C-x C-b") nil)
+
 ;; Override yes-or-no-p's definition by making yes-or-no-p an alias
 ;; for y-or-n-p.  This way, when yes-or-no-p is called, y-or-n-p will
 ;; actually be called, and I can reply with y/SPC or n/DEL instead of
