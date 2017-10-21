f9d2af16a7f23a365135584bd4daf39df7016106
Raghu V. Rao <raghu.v.rao@gmail.com>
2017-10-21 11:35:36 -0700

    emacs: more color tweaking

--- emacs/init.el
+++ emacs/init.el
@@ -426,12 +426,15 @@ Do so only when Emacs is running on a graphic display."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((type x) (class color) (background dark)) (:background "gray10" :foreground "#CCBC8B" :height 90)) (((type ns) (class color) (background dark)) (:background "gray10" :foreground "#CCBC8B" :height 110))))
+ '(default ((((type x) (class color) (background dark)) (:background "gray10" :foreground "wheat3" :height 90)) (((type ns) (class color) (background dark)) (:background "gray10" :foreground "wheat3" :height 110))))
+ '(diff-file-header ((((type x ns) (class color) (background dark)) (:background "grey60" :foreground "grey20" :weight bold))))
+ '(diff-header ((((type x ns) (class color) (background dark)) (:background "grey24" :foreground "grey70"))))
  '(fringe ((t (:inherit default))))
  '(hl-line ((((type x ns) (class color) (background light)) (:background "azure2")) (((type x ns) (class color) (background dark)) (:background "gray23"))))
  '(isearch ((((type x ns) (class color) (background light)) (:background "plum2" :foreground "black"))))
  '(lazy-highlight ((((type x ns) (class color) (background dark)) (:background "RoyalBlue4" :foreground "LightSkyBlue"))))
- '(region ((((type x ns) (class color) (background light)) (:background "LightGoldenrod2"))))
+ '(match ((((type x ns) (class color) (background dark)) (:background "orange2" :foreground "black"))))
+ '(region ((((type x ns) (class color) (background light)) (:background "LightGoldenrod2" :foreground "black")) (((type x ns) (class color) (background dark)) (:background "wheat3" :foreground "gray10"))))
  '(show-paren-match ((((type x ns) (class color) (background dark)) (:background "green4" :foreground "white")))))
 
 (require 'server)
