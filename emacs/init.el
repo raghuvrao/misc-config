7db3ed2a5936e022d288c5902e91f0b5d75a9e83
Raghu V. Rao <raghu.v.rao@gmail.com>
2020-04-17 20:01:58 -0700

    Revert cursor-blinking to defaults

--- emacs/init.el
+++ emacs/init.el
@@ -17,8 +17,8 @@
 (when (fboundp #'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp #'tooltip-mode) (tooltip-mode -1))
 
-(blink-cursor-mode 1)
 (column-number-mode 1)
+
 (show-paren-mode 1)
 
 ;; Various programs (e.g. git, man) like to send their stdout through a
