07fd8fd64f62f79b9087327e13003d7ca2877152
Raghu V. Rao <raghu.v.rao@gmail.com>
2020-07-08 11:52:24 -0700

    Emacs: do not ring bell
    Emacs rings the bell for way too many things.

--- emacs/init.el
+++ emacs/init.el
@@ -418,6 +418,7 @@ UNIX timestamp."
     (("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/"))))
  '(package-enable-at-startup nil)
+ '(ring-bell-function (quote ignore))
  '(save-place-mode t)
  '(scroll-bar-mode nil)
  '(scroll-conservatively 5)
