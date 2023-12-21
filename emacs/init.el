5c0d19ed50af2baf19fe32b85f55682131cf79a3
Raghu V. Rao <raghu.v.rao@gmail.com>
2023-12-21 00:49:29 -0800

    Emacs: do not blink the cursor

--- emacs/init.el
+++ emacs/init.el
@@ -663,6 +663,7 @@ Address the above through this function."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(backward-delete-char-untabify-method nil)
+ '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(comint-buffer-maximum-size 10240)
  '(confirm-kill-emacs 'y-or-n-p)
